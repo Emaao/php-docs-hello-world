dae713a862685b51176ac3630f61e06e6c020db1
Emaao <azizimane953@gmail.com>
2023-12-09 16:40:46 +0100

    uploaded oneroom

--- oneroom.php
+++ oneroom.php
@@ -1,8 +1,37 @@
 <?php
-include 'db.php';
+include 'rooms.php';
 
 // Get RoomNumber from the query parameters
 $roomNumber = $_GET['RoomNumber'];
+
+// Check if the form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['reserve'])) {
+    try {
+        // Update the availability in the database
+        $sqlUpdate = "UPDATE Salles SET Availability = 0 WHERE RoomNumber = :roomNumber";
+        $stmtUpdate = $conn->prepare($sqlUpdate);
+        $stmtUpdate->bindParam(':roomNumber', $roomNumber);
+        $stmtUpdate->execute();
+
+        // Fetch updated room details from the database
+        $sql = "SELECT * FROM Salles WHERE RoomNumber = :roomNumber";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':roomNumber', $roomNumber);
+        $stmt->execute();
+        $room = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // Return updated availability as a JSON response
+        header('Content-Type: application/json');
+        echo json_encode(['availability' => $room['Availability']]);
+        exit();
+    } catch (PDOException $e) {
+        // Log the error
+        error_log("Error updating availability: " . $e->getMessage());
+        // Return an error response if needed
+        http_response_code(500);
+        exit();
+    }
+}
 
 // Fetch room details from the database
 $sql = "SELECT * FROM Salles WHERE RoomNumber = :roomNumber";
@@ -26,20 +55,17 @@ $room = $stmt->fetch(PDO::FETCH_ASSOC);
     <div>
         <h1>Room Details</h1>
         <p>Room Number: <?php echo $room['RoomNumber']; ?></p>
-        <p>Availability: <?php echo $room['Availability']; ?></p>
+        <p>Availability: <span id="availability"><?php echo $room['Availability']; ?></span></p>
 
         <!-- Display the room image from Blob Storage -->
         <img src="<?php echo $room['imagePath']; ?>" alt="Room Image">
 
-
-        <!-- Reserve button -->
-        <?php
-            if ($room['Availability'] == 1) {
-                echo '<button onclick="reserveRoom(' . $room['RoomNumber'] . ')">Reserve Room</button>';
-            } else {
-                echo '<button disabled>Room Reserved</button>';
-            }
-        ?>
+        <!-- Reserve button and form -->
+        <form id="reserveForm">
+            <button type="button" onclick="reserveRoom(<?php echo $room['RoomNumber']; ?>)">
+                Reserve Room
+            </button>
+        </form>
     </div>
 
     <!-- JavaScript function to handle room reservation -->
@@ -49,10 +75,18 @@ $room = $stmt->fetch(PDO::FETCH_ASSOC);
             // Make an AJAX request to update availability
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function () {
-                if (this.readyState == 4 && this.status == 200) {
-                    // Update the button text and disable the button
-                    document.querySelector("button").innerText = "Room Reserved";
-                    document.querySelector("button").disabled = true;
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        // Update the button text and disable the button
+                        document.querySelector("button").innerText = "Room Reserved";
+                        document.querySelector("button").disabled = true;
+
+                        // Update the availability in the page
+                        document.getElementById("availability").innerText = "0";
+                    } else {
+                        // Log an error if the request is not successful
+                        console.error("Error updating availability");
+                    }
                 }
             };
             xhttp.open("POST", "oneroom.php?RoomNumber=" + RoomNumber, true);
